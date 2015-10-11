dd6471eff33b37e1074ac7596badc526fd4208f9
Maris Malva <mmalva@klass1-13-l.intra.itcollege.ee>
2015-10-11 14:32:03 +0300

    ristmiku lahendus

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,8 +1,83 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lib.Foor;
+
 /**
  * Täpselt nagu oli fooriga, on nüüd ristmikuga. Kuidas esiteks
  * ristmik üles ehitada loe Foori README.md-st: https://github.com/KristerV/javaHarjutused
  */
-public class Peamurdja2_ristmik {
+public class Peamurdja2_ristmik extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor1 = new Foor(Foor.YLEMINE, primaryStage);
+        Foor foor2 = new Foor(Foor.ALUMINE, primaryStage);
+        Foor foor3 = new Foor(Foor.VASAK, primaryStage);
+        Foor foor4 = new Foor(Foor.PAREM, primaryStage);
+
+
+
+
+            tsykkel(foor1, 0);
+            tsykkel(foor2, 0);
+            tsykkel(foor3, 1);
+            tsykkel(foor4, 1);
+
+
+
+    }
+
+
+
+    public void tsykkel(Foor foor, int suund) {
+        int count = suund;
+        while (count < 10) {
+            if (count != 0) {
+
+
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaKollast();
+                foor.paus(0.5);
+                foor.vahetaKollast();
+                foor.paus(0.5);
+                foor.vahetaPunast();
+                foor.paus(1.5);
+                foor.vahetaPunast();
+                foor.paus(0.5);
+
+
+            }
+
+            foor.vahetaPunast();
+            foor.paus(1.5);
+            foor.vahetaPunast();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+
+            count++;
+
+
+        }
+    }
+
+
 }
+
